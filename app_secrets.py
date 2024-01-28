0d7abe60bac871c167dbe444bf2926c8deadd979
Paul Imoke <pauleke65@gmail.com>
2024-01-28 21:10:42 +0100

    fix: keys

--- app_secrets.py
+++ app_secrets.py
@@ -1,12 +1,18 @@
 from dotenv import dotenv_values
+import os
 
 
 env = dotenv_values()
 
 
+def get_env(key):
+    """Get environment variable."""
+    return os.getenv(key, default=env.get(key))
+
+
 class AppSecrets:
     """Static Class to store secrets."""
 
-    github_access_token = env.get("GITHUB_ACCESS_TOKEN")
-    github_username = env.get("GITHUB_USERNAME")
-    environment = env.get("ENVIRONMENT")
+    github_access_token = get_env("GITHUB_ACCESS_TOKEN")
+    github_username = get_env("GITHUB_USERNAME")
+    environment = get_env("ENVIRONMENT")
